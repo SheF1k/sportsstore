b17a7335c684687a0664e062a1a923042e91ce9f
SheF1k <kissiarule@gmail.com>
2020-11-29 18:47:14 +0200

    add cart summary and possibility to add product to card

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -14,7 +14,12 @@ export class ProductList extends Component {
           {product.name}
           <span className="badge badge-pill badge-primary float-right">${product.price.toFixed(2)}</span>
         </h4>
-        <div className="card-text bg-white p-1">{product.description} </div>
+        <div className="card-text bg-white p-1">
+          {product.description} 
+          <button className="btn btn-success btn-sm float-right" onClick={() => this.props.addToCart(product)}>
+            Add To Cart
+          </button>
+        </div>
       </div>
     ))
   }
--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { CategoryNavigation } from './CategoryNavigation'
 import { ProductList } from './ProductList'
+import { CartSummary } from './CartSummary'
+
 export class Shop extends Component {
   render() {
     return (
@@ -8,6 +10,7 @@ export class Shop extends Component {
         <div className="row">
           <div className="col bg-dark text-white">
             <div className="navbar-brand">SPORTS STORE</div>
+            <CartSummary {...this.props} />
           </div>
         </div>
         <div className="row">
@@ -15,7 +18,7 @@ export class Shop extends Component {
             <CategoryNavigation baseUrl="/shop/products" categories={this.props.categories} />
           </div>
           <div className="col-9 p-2">
-            <ProductList products={this.props.products} />
+            <ProductList products={this.props.products} {...this.props} />
           </div>
         </div>
       </div>
