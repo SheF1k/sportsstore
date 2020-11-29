5f70f17b9c6098ec9ad8d94b5100cfdc800618ad
SheF1k <kissiarule@gmail.com>
2020-11-29 21:27:11 +0200

    replace mocked data with data from server

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { CartReducer } from './CartReducer'
 import { ShopReducer } from './ShopReducer'
 import { CommonReducer } from './CommonReducer'
+import { asyncActions } from './AsyncMiddleware'
 
-export const SportsStoreDataStore = createStore(
-  CommonReducer(ShopReducer, CartReducer),
+const middlewareEnhancer = applyMiddleware(asyncActions)
+const composedEnhancers = compose(
+  middlewareEnhancer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
+
+export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer), undefined, composedEnhancers)
