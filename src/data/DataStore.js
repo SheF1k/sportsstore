09e2b07f418c8e1b8ae13bbba0be85359d695bbb
SheF1k <kissiarule@gmail.com>
2020-11-29 18:33:39 +0200

    add store for cart functionality

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,9 @@
 import { createStore } from 'redux'
+import { CartReducer } from './CartReducer'
 import { ShopReducer } from './ShopReducer'
+import { CommonReducer } from './CommonReducer'
 
-export const SportsStoreDataStore = createStore(ShopReducer)
+export const SportsStoreDataStore = createStore(
+  CommonReducer(ShopReducer, CartReducer),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
